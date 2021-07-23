8373b5e7f82f48bb41ca64a676b73e96ad3e89f5
daavera <da.avila@uniandes.edu.co>
2021-07-23 17:03:08 -0500

    Botones secuenciales

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ linterna_logo =  Image.open(IMAGES_DIR/'linterna_logo.jpg')
 header = st.beta_container()
 body = st.beta_container()
 
-#st.beta_set_page_config(layout="wide")
-
 with header:
 
     st.image(linterna_logo)
@@ -38,7 +36,7 @@ with body:
     st.write('## Cargar el diccionario')
 
     url = 'https://docs.google.com/spreadsheets/d/1iilHcaQqvJj6ShrnIbRFlKeqjaCnoNxoza11WGuYwps/edit#gid=0'
-    url = st.text_input('Inserte URL del diccionario:', url)
+    url = st.text_input('Ingrese URL del diccionario:', url)
 
     try:
         spreadsheet = SpreadSheet(SCOPES, st.secrets["gcp_service_account"], url)
@@ -47,38 +45,57 @@ with body:
     except:
         st.error('Ingrese una URL válida.')
 
-    #SELECT DATA
-    st.write('## Seleccionar columnas')
-    usr_cols = st.multiselect('Columnas' ,cols, default=cols, key='General')
+    if 'pressed_1st_button' not in st.session_state:
+        st.session_state.pressed_1st_button = False
 
-    st.write(df[usr_cols])
+    if st.button("Confrimar", key= 'confirmar1')  or st.session_state.pressed_1st_button:
+        st.session_state.pressed_1st_button = True #guardar sesion
 
-    #GROUP BY COLUMN
-    st.write('## Agrupar columnas')
-    col1, col2 = st.beta_columns(2)
-    group1 = col1.multiselect('Grupo 1', usr_cols, default=None, key='group1')
-    group2 = col2.multiselect('Grupo 2', list(set(usr_cols).difference(group1)), default=None, key='group2')
+        #SELECT DATA
+        st.write('## Seleccionar columnas')
+        usr_cols = st.multiselect('Columnas' ,cols, default=cols, key='General')
 
-    query = Query(df[usr_cols])
-    query.preprocess_dictionary()
+        st.write(df[usr_cols])
 
-    for g in [group1,group2]:
-        if len(g) != 0:
-            query.combine_columns(g)
+        if 'pressed_2nd_button' not in st.session_state:
+            st.session_state.pressed_2nd_button = False
+
+        if st.button("Confrimar", key= 'confirmar2')  or st.session_state.pressed_2nd_button:
+            st.session_state.pressed_2nd_button = True #guardar sesion
+
+            #GROUP BY COLUMN
+            st.write('## Agrupar columnas')
+            col1, col2 = st.beta_columns(2)
+            group1 = col1.multiselect('Grupo 1', usr_cols, default=None, key='group1')
+            group2 = col2.multiselect('Grupo 2', list(set(usr_cols).difference(group1)), default=None, key='group2')
+
+            query = Query(df[usr_cols])
+            query.preprocess_dictionary()
+
+            for g in [group1,group2]:
+                if len(g) != 0:
+                    query.combine_columns(g)
 
 
-    # PRINT LOGICAL EXPRESION
-    st.write('## Estructura lógica ')
-    st.write(query.get_logic_expression())
+            # PRINT LOGICAL EXPRESION
+            st.write('## Estructura lógica ')
+            st.write(query.get_logic_expression())
 
+            if 'pressed_3rd_button' not in st.session_state:
+                st.session_state.pressed_3rd_button = False
 
-    if st.checkbox('Generar query', value=False):
-        #PRINT & APPEND QUERY
-        st.write('## Query ' + ':snake:')
+            if st.button('Generar query') or st.session_state.pressed_3rd_button:
+                st.session_state.pressed_3rd_button = True #guardar sesion
+                #PRINT & APPEND QUERY
+                st.write('## Query ' + ':snake:')
 
-        query_text = query.get_query()
-        st.write(query_text)
+                query_text = query.get_query()
+                st.write(query_text)
 
-        if st.button('Agregar al documento de Drive'):
-            spreadsheet.append_query_to_spreadsheet(query_text)
-            st.write('## ¡Listo! :crocodile:')
+                st.write('## Agregar al documento de Drive')
+
+                query_name = st.text_input('Ingrese nombre de la query:',  'Query', key='query_name')
+
+                if st.button('Confirmar'):
+                    spreadsheet.append_query_to_spreadsheet(query_text, query_name)
+                    st.write('## ¡Listo! :crocodile:')
